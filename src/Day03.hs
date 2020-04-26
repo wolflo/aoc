44ed6d8090a3b8690323457ce0a742f2c1bd80c9
wolflo <wardnick@protonmail.com>
2020-04-26 12:17:41 +0400

    Day 3 p1 improved

--- src/Day03.hs
+++ src/Day03.hs
@@ -1,18 +1,18 @@
 module Day03 where
 
-import Prelude
-import Linear.V2
-import Data.List (intersect)
-import Data.List.Split (splitOn)
+import           Prelude
+import           Linear.V2
+import           Data.List (intersect)
+import           Data.List.Split (splitOn)
+import qualified Data.Set as S
 
--- import Diagrams.TwoD.Vector
-import Lib
+import           Lib
 
 ----- Part 1
-p1 :: IO Int
+p1 :: IO ()
 p1 = do
   contents <- readFile "src/inputs/day03.txt"
-  return $ crossDist contents
+  print $ crossDist . toDisps $ lines contents
 
 type Pos = V2 Int           -- a position in our 2d space
 type Disp = (Int, V2 Int)   -- a displacement. (Magnitude, unit vector)
@@ -21,20 +21,19 @@ type Path = [Pos]
 origin :: Pos
 origin = V2 2 1
 
-crossDist :: String -> Int
-crossDist contents = minimum . map (mannDist origin) . tail $ intersect path1 path2
+crossDist :: [[Disp]] -> Int
+crossDist disps = minimum . map (mannDist origin) $ meets
   where
-    disps = toDisps $ words contents
-    path1 = totalPath origin $ head disps
-    path2 = totalPath origin $ last disps
+    meets = S.toList $ S.intersection (path $ head disps) (path $ last disps)
+    path = S.fromList . tail . totalPath origin
 
 -- |x2 - x1| + |y2 - y1|
 mannDist :: Pos -> Pos -> Int
 mannDist (V2 x1 y1) (V2 x2 y2) = abs (x2 - x1) + abs (y2 - y1)
 
 totalPath :: Pos -> [Disp] -> Path
-totalPath pos []           = []
-totalPath pos (disp:disps) = step ++ totalPath endStep disps
+totalPath pos []           = [V2 0 0]   -- a hack to not fail on final tail
+totalPath pos (disp:disps) = step ++ (tail $ totalPath endStep disps)
   where
     step    = toPath pos disp
     endStep = last step
