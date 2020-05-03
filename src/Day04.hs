c3074db56fdc410f58f77d8d3d52abf883af5d96
wolflo <wardnick@protonmail.com>
2020-05-03 19:05:24 +0400

    Day 4 p2

--- src/Day04.hs
+++ src/Day04.hs
@@ -1,6 +1,7 @@
 module Day04 where
 
 import           Prelude
+import           Data.List (intersect)
 import           Data.List.Split (splitOn)
 import           Text.Read (readMaybe)
 import           Data.Char (ord)
@@ -39,3 +40,18 @@ monotonic = all inc . consec . map ord
 hasDub :: String -> Bool
 hasDub = any match . consec
   where match (a,b) = a == b
+
+----- Part 2
+p2 :: IO Int
+p2 = do
+  contents <- readFile inputPath
+  return . length . strictStrain $ parseRange contents
+
+strictStrain :: [Int] -> [Int]
+strictStrain = filter checkAll
+  where
+    checkAll xs = digits (show xs) && monotonic (show xs) && strictDub (show xs)
+
+strictDub :: String -> Bool
+strictDub ds = any match $ consec ds
+  where match (a,b) = a == b && length (ds `intersect` (replicate 3 a)) < 3
